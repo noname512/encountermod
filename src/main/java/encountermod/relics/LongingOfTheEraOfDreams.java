9f92c7d6cad62088ffa352c333d9f8923e4c3fcd
noname512 <zxononame@gmail.com>
2025-03-11 19:47:34 +0800

    美愿part2

--- src/main/java/encountermod/relics/LongingOfTheEraOfDreams.java
+++ src/main/java/encountermod/relics/LongingOfTheEraOfDreams.java
@@ -7,20 +7,21 @@ import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
 import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.red.FiendFire;
-import com.megacrit.cardcrawl.cards.tempCards.Shiv;
-import com.megacrit.cardcrawl.characters.Defect;
-import com.megacrit.cardcrawl.characters.Ironclad;
-import com.megacrit.cardcrawl.characters.TheSilent;
-import com.megacrit.cardcrawl.characters.Watcher;
+import com.megacrit.cardcrawl.cards.blue.*;
+import com.megacrit.cardcrawl.cards.colorless.Bite;
+import com.megacrit.cardcrawl.cards.green.*;
+import com.megacrit.cardcrawl.cards.red.*;
+import com.megacrit.cardcrawl.cards.tempCards.*;
+import com.megacrit.cardcrawl.characters.*;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.RelicStrings;
-import com.megacrit.cardcrawl.potions.AbstractPotion;
-import com.megacrit.cardcrawl.potions.PotionSlot;
+import com.megacrit.cardcrawl.potions.*;
 import com.megacrit.cardcrawl.relics.*;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import com.megacrit.cardcrawl.rooms.TreasureRoom;
+import com.megacrit.cardcrawl.shop.ShopScreen;
 import encountermod.EncounterMod;
 import encountermod.patches.RefreshPatch;
 import encountermod.reward.ExtraRelicReward;
@@ -42,8 +43,8 @@ public class LongingOfTheEraOfDreams extends CustomRelic {
     @Override
     public void onEquip() {
         AbstractDungeon.player.gainGold(50);
-        RefreshPatch.roomWeight.put("Treasure", 6);
-        RefreshPatch.totalWeight = 16;
+        RefreshPatch.roomWeight.put("Treasure", 8);
+        RefreshPatch.totalWeight = 18;
     }
 
     @Override
@@ -106,6 +107,36 @@ public class LongingOfTheEraOfDreams extends CustomRelic {
                             lst.add(new PenNib()); // 钢笔尖
                             lst.add(new Nunchaku()); // 双节棍
                         }
+                        if (AbstractDungeon.player.hasRelic(WhiteBeast.ID)) {
+                            lst.add(new ToyOrnithopter()); // 玩具扑翼飞机
+                        }
+                        if (AbstractDungeon.player instanceof Watcher && calcMantra() > 0) {
+                            lst.add(new Damaru()); // 手摇鼓
+                        }
+                        if (ShopScreen.actualPurgeCost >= 175) {
+                            lst.add(new SmilingMask()); // 微笑面具
+                        }
+                        if (calcUnupgradeAttack() >= 4) {
+                            lst.add(new Whetstone()); // 磨刀石
+                        }
+                        if (calcUnupgradeSkill() >= 4) {
+                            lst.add(new WarPaint()); // 战纹涂料
+                        }
+                        if (AbstractDungeon.player instanceof Defect && calcChannel() >= 3) {
+                            lst.add(new DataDisk()); // 数据磁盘
+                        }
+                        if (AbstractDungeon.actNum == 1) {
+                            lst.add(new CeramicFish()); // 陶瓷小鱼
+                        }
+                        if (calBlockCard() >= AbstractDungeon.player.masterDeck.size() * 0.4) {
+                            lst.add(new OddlySmoothStone()); // 意外光滑的石头
+                        }
+                        if (calcPoisonTime() >= 16) {
+                            lst.add(new SneckoSkull()); // 异蛇头骨
+                        }
+                        if (calcLittleDamage() >= 4) {
+                            lst.add(new Boot()); // 发条靴
+                        }
                         break;
                     case UNCOMMON:
                         if (AbstractDungeon.player.masterDeck.size() <= 7) {
@@ -135,13 +166,13 @@ public class LongingOfTheEraOfDreams extends CustomRelic {
                         if (AbstractDungeon.actNum == 1 && (AbstractDungeon.player instanceof Ironclad)) {
                             lst.add(new MoltenEgg2()); // 熔火之蛋
                         }
-                        if (AbstractDungeon.actNum == 1 && (AbstractDungeon.player instanceof Defect || AbstractDungeon.player.getClass().getName().contains("Nearl"))) {
+                        if ((AbstractDungeon.actNum <= 2 && AbstractDungeon.player instanceof Defect) || (AbstractDungeon.actNum == 1 && AbstractDungeon.player.getClass().getName().contains("Nearl"))) {
                             lst.add(new FrozenEgg2()); // 冻结之蛋
                         }
                         if (EncounterMod.ideaCount >= 4) {
                             lst.add(new RevenantRemains()); // 死魂灵残躯
                         }
-                        if (calcPowerCard() >= AbstractDungeon.player.masterDeck.size() * 0.3) {
+                        if (calcPowerCard() >= AbstractDungeon.player.masterDeck.size() * 0.2) {
                             lst.add(new MummifiedHand()); // 干瘪之手
                         }
                         if (calcSkillCard() >= AbstractDungeon.player.masterDeck.size() * 0.7) {
@@ -158,6 +189,19 @@ public class LongingOfTheEraOfDreams extends CustomRelic {
                         if (AbstractDungeon.player.masterDeck.size() >= 25) {
                             lst.add(new EternalFeather()); // 永恒羽毛
                         }
+                        if (AbstractDungeon.player instanceof Defect && checkSymbioticVirus()) {
+                            lst.add(new SymbioticVirus()); // 共生病毒
+                        }
+                        if (AbstractDungeon.player instanceof TheSilent && checkNinjaScroll()) {
+                            lst.add(new NinjaScroll()); // 忍术卷轴
+                        }
+                        if (AbstractDungeon.actNum < 24) {
+                            lst.add(new SingingBowl()); // 颂钵
+                            lst.add(new QuestionCard()); // 问号牌
+                        }
+                        if (AbstractDungeon.player instanceof Defect && checkCables()) {
+                            lst.add(new GoldPlatedCables()); // 镀金缆线
+                        }
                         break;
                     case RARE:
                         if (calcExhaust() >= 5) {
@@ -172,11 +216,12 @@ public class LongingOfTheEraOfDreams extends CustomRelic {
                         if (AbstractDungeon.player.maxHealth < 50) {
                             lst.add(new Mango()); // 芒果
                         }
-                        if (EncounterMod.ideaCount <= 1 && AbstractDungeon.floorNum <= 25) {
+                        if (EncounterMod.ideaCount <= 1 && AbstractDungeon.floorNum < 24) {
                             lst.add(new SpiritHunterEarl()); // 探灵伯爵
                         }
                         if (calcDiscard() >= 3) {
                             lst.add(new ToughBandages()); // 结实绷带
+                            lst.add(new Tingsha()); // 铜钹
                         }
                         if (calcExhaustOthers() >= 3 && AbstractDungeon.player instanceof Ironclad) {
                             lst.add(new CharonsAshes()); // 卡戎之灰
@@ -193,13 +238,36 @@ public class LongingOfTheEraOfDreams extends CustomRelic {
                         if (calcRetain() >= 3) {
                             lst.add(new CloakClasp()); // 斗篷扣
                         }
+                        if (AbstractDungeon.actNum <= 2 && Settings.hasRubyKey) {
+                            lst.add(new Girya()); // 壶铃
+                            lst.add(new Shovel()); // 铲子
+                        }
+                        if (AbstractDungeon.floorNum < 24) {
+                            lst.add(new PrayerWheel()); // 转经轮
+                        }
+                        if (AbstractDungeon.player.gold <= 300) {
+                            lst.add(new OldCoin()); // 古钱币
+                        }
+                        if (AbstractDungeon.floorNum < 40) {
+                            lst.add(new WingBoots()); // 羽翼之靴
+                        }
+                        if (AbstractDungeon.player instanceof Ironclad && checkMagicFlower()) {
+                            lst.add(new MagicFlower()); // 魔法花
+                        }
+                        if (calcPoisonNum() >= 16) {
+                            lst.add(new TheSpecimen()); // 生物样本
+                        }
+                        if (calcScry() >= 4) {
+                            lst.add(new GoldenEye()); // 黄金眼
+                        }
                         break;
                     default:
                         return SpireReturn.Continue();
                 }
                 lst.removeIf(r -> AbstractDungeon.player.hasRelic(r.relicId));
                 if (lst.isEmpty()) {
-                    lst.add(new CultistMask()); // 邪教徒头套
+                    return SpireReturn.Continue();
+//                    lst.add(new CultistMask()); // 邪教徒头套
                 }
                 return SpireReturn.Return(lst.get(AbstractDungeon.relicRng.random(lst.size() - 1)));
             }
@@ -310,12 +378,13 @@ public class LongingOfTheEraOfDreams extends CustomRelic {
         private static float calcZeroCostCard() {
             int cnt = 0;
             for (AbstractCard c : AbstractDungeon.player.masterDeck.group)
-                if (c.cardsToPreview instanceof Shiv || (c.type == AbstractCard.CardType.ATTACK &&
-                        (c.rawDescription.contains("耗能") && c.rawDescription.contains("减少")) ||
+                if ((c.cardsToPreview instanceof Shiv && !(c instanceof Accuracy)) || (c.type == AbstractCard.CardType.ATTACK &&
+                        ((c.rawDescription.contains("耗能") && c.rawDescription.contains("减少")) ||
                         ((c.rawDescription.contains("cost") || c.rawDescription.contains("Cost")) &&
                                 (c.rawDescription.contains("less") || c.rawDescription.contains("Less") || c.rawDescription.contains("reduce") || c.rawDescription.contains("Reduce"))) ||
                         (c.rawDescription.contains("耗能") && c.rawDescription.contains("減少")) ||
-                        ((c.rawDescription.contains("コスト") || c.rawDescription.contains("消費")) && (c.rawDescription.contains("下がる") || c.rawDescription.contains("-1") || c.rawDescription.contains("減少")))))
+                        ((c.rawDescription.contains("コスト") || c.rawDescription.contains("消費")) && (c.rawDescription.contains("下がる") || c.rawDescription.contains("-1") || c.rawDescription.contains("減少"))) ||
+                        c.cost == 0)))
                     cnt++;
             return cnt;
         }
@@ -333,6 +402,122 @@ public class LongingOfTheEraOfDreams extends CustomRelic {
                 if (c.selfRetain)
                     cnt++;
             return cnt;
+        }
+
+        private static int calcMantra() {
+            int cnt = 0;
+            for (AbstractCard c : AbstractDungeon.player.masterDeck.group)
+                if (c.rawDescription.contains("真言") || c.rawDescription.contains("Mantra") || c.rawDescription.contains("マントラ"))
+                    cnt++;
+            return cnt;
+        }
+
+        private static int calcUnupgradeAttack() {
+            int cnt = 0;
+            for (AbstractCard c : AbstractDungeon.player.masterDeck.group)
+                if (c.type == AbstractCard.CardType.ATTACK && !c.upgraded)
+                    cnt++;
+            return cnt;
+        }
+
+        private static int calcUnupgradeSkill() {
+            int cnt = 0;
+            for (AbstractCard c : AbstractDungeon.player.masterDeck.group)
+                if (c.type == AbstractCard.CardType.SKILL && !c.upgraded)
+                    cnt++;
+            return cnt;
+        }
+
+        private static int calcChannel() {
+            int cnt = 0;
+            for (AbstractCard c : AbstractDungeon.player.masterDeck.group)
+                if ((c.rawDescription.contains("生成") && c.rawDescription.contains("充能球") && !(c instanceof Blizzard)) ||
+                        (c.rawDescription.contains("Channel") && !c.rawDescription.contains("Channeled")) ||
+                        (c.rawDescription.contains("生成") && !(c instanceof Blizzard) &&
+                                (c.rawDescription.contains("ライトニング") /* 闪电 */ ||
+                                c.rawDescription.contains("プラズマ") /* 等离子 */ ||
+                                c.rawDescription.contains("フロスト") /* 冰霜 */ ||
+                                c.rawDescription.contains("ダーク") /* 黑暗 */)))
+                    cnt++;
+            return cnt;
+        }
+
+        private static boolean checkSymbioticVirus() {
+            for (AbstractCard c : AbstractDungeon.player.masterDeck.group)
+                if ((c instanceof Darkness && c.upgraded) || (c instanceof Recursion) || (c instanceof Loop))
+                    return true;
+            return false;
+        }
+
+        private static boolean checkNinjaScroll() {
+            if (AbstractDungeon.player.hasRelic(BagOfPreparation.ID)) return false;
+            if (AbstractDungeon.player.hasRelic(Shuriken.ID)) return true;
+            if (AbstractDungeon.player.hasRelic(OrnamentalFan.ID)) return true;
+            if (AbstractDungeon.player.hasRelic(Kunai.ID)) return true;
+            for (AbstractCard c : AbstractDungeon.player.masterDeck.group)
+                if (c instanceof Accuracy)
+                    return true;
+            return false;
+        }
+
+        private static boolean checkMagicFlower() {
+            if (AbstractDungeon.player.hasRelic(BlackBlood.ID)) return true;
+            if (AbstractDungeon.player.hasRelic(MeatOnTheBone.ID)) return true;
+            for (AbstractCard c : AbstractDungeon.player.masterDeck.group)
+                if (c instanceof Reaper || c instanceof Bite)
+                    return true;
+            return false;
+        }
+
+        private static int calBlockCard() {
+            int cnt = 0;
+            for (AbstractCard c : AbstractDungeon.player.masterDeck.group)
+                if (c.baseBlock != -1)
+                    cnt++;
+            return cnt;
+        }
+
+        private static int calcPoisonNum() {
+            int cnt = 0;
+            for (AbstractCard c : AbstractDungeon.player.masterDeck.group) {
+                if (c instanceof BouncingFlask) cnt += 3 * c.baseMagicNumber;
+                if (c instanceof CorpseExplosion || c instanceof CripplingPoison || c instanceof DeadlyPoison || c instanceof PoisonedStab) cnt += c.baseMagicNumber;
+                if (c instanceof Envenom || c instanceof NoxiousFumes) cnt += 16;
+            }
+            return cnt;
+        }
+
+        private static int calcPoisonTime() {
+            int cnt = 0;
+            for (AbstractCard c : AbstractDungeon.player.masterDeck.group) {
+                if (c instanceof BouncingFlask) cnt += 3;
+                if (c instanceof CorpseExplosion || c instanceof CripplingPoison || c instanceof DeadlyPoison || c instanceof PoisonedStab || c instanceof Catalyst) cnt++;
+                if (c instanceof Envenom || c instanceof NoxiousFumes) cnt += 3;
+            }
+            return cnt;
+        }
+
+        private static int calcLittleDamage() {
+            int cnt = 0;
+            for (AbstractCard c : AbstractDungeon.player.masterDeck.group)
+                if ((c.cardsToPreview instanceof Shiv && !(c instanceof Accuracy)) || (c.type == AbstractCard.CardType.ATTACK && c.baseDamage >= 0 && c.baseDamage <= 4))
+                    cnt++;
+            return cnt;
+        }
+
+        private static int calcScry() {
+            int cnt = 0;
+            for (AbstractCard c : AbstractDungeon.player.masterDeck.group)
+                if (c.rawDescription.contains("预见 !M!") || c.rawDescription.contains("Scry !M!") || c.rawDescription.contains("預見 !M!") || c.rawDescription.contains("占術 !M!"))
+                    cnt++;
+            return cnt;
+        }
+
+        private static boolean checkCables() {
+            for (AbstractCard c : AbstractDungeon.player.masterDeck.group)
+                if (c instanceof Loop || c.rawDescription.contains("获得 !M! 点 集中") || c.rawDescription.contains("Gain !M! Focus") || c.rawDescription.contains("獲得 !M! 點 集中") || c.rawDescription.contains("集中力 !M! を得る"))
+                    return true;
+            return false;
         }
     }
 
